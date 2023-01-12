3dbdedc7dbf965381efb5d674c86057ced1c3cd1
yongning0310 <85162884+yongning0310@users.noreply.github.com>
2023-01-12 10:37:30 +0800

    initial commit

---
+++ src/main/java/com/example/fusionwallet/FusionWalletApplication.java
@@ -0,0 +1,13 @@
+package com.example.fusionwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusionWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FusionWalletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fusionwallet/FusionWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fusionwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FusionWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
