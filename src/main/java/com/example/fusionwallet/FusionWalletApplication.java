7f06b56e15d7eccd3ce254f629d5ba0427d5a1c2
yongning0310 <85162884+yongning0310@users.noreply.github.com>
2023-01-16 12:46:25 +0800

    string to double bug

--- src/main/java/com/example/fusionwallet/FusionWalletApplication.java
+++ src/main/java/com/example/fusionwallet/FusionWalletApplication.java
@@ -1,8 +1,14 @@
 package com.example.fusionwallet;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@SpringBootConfiguration
+@EnableConfigurationProperties
+@Configuration
 @SpringBootApplication
 public class FusionWalletApplication {
 
